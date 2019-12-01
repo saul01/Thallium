f5da07551e0835b832ecba0771b205248a88f2f3
Saul Rodriguez <saulrodriguez@Sauls-MacBook-Pro.local>
2019-11-30 21:32:44 -0800

    adding the app project

---
+++ App/ThalliumApp/ThalliumApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ThalliumApp
+//
+//  Created by Saul Rodriguez on 11/30/19.
+//  Copyright © 2019 Saul Rodriguez. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ App/ThalliumApp/ThalliumApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ThalliumApp
+//
+//  Created by Saul Rodriguez on 11/30/19.
+//  Copyright © 2019 Saul Rodriguez. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
